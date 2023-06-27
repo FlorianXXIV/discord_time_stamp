f3fc8f31d0d3bfbddb864aa6b9f0bac03a416672
Florian Kokotz <f.kokotz@tu-braunschweig.de>
2023-06-27 22:35:53 +0200

    Updated project setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::ffi::c_int;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the Number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    let number: c_int;
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read Line");
+
+    println!("You guessed: {guess}");
 }
