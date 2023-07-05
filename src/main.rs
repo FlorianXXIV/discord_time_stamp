414bf92f8590c700cb0513ad01656f7bcc5e3392
Florian Kokotz <f.kokotz@tu-braunschweig.de>
2023-07-05 10:50:27 +0200

    changed UtcOffset::from_hms to UtcOffset::parse to simplify code structure. added ability to put in your own date

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use time::{Date, Time, UtcOffset};
 use time::format_description::FormatItem;
 use time::macros::format_description;
+use std::io;
 
 const DATE_FORMAT:&[FormatItem] = format_description!("[year]-[month]-[day]");
 const TIME_FORMAT:&[FormatItem] = format_description!("[hour]:[minute]");
+const OFFSET_FORMAT:&[FormatItem] = format_description!("[offset_hour]:[offset_minute]");
 fn main() {
-    let date = "2023-07-05";
-    let time = "09:45";
-    let offset_h = 2;
-    let offset_m = 0;
+    let mut buffer = String::new();
 
-    let unix = unix_from_datetime(date, time, offset_h, offset_m);
+    println!("Input date as [year]-[month]-[day]");
+    io::stdin().read_line(&mut buffer).expect("failed to read line");
+    let date = buffer.clone();
+    buffer.clear();
+    println!("Input time as [hour]:[minute]");
+    io::stdin().read_line(&mut buffer).expect("failed to read line");
+    let time = buffer.clone();
+    buffer.clear();
+    println!("Input timezone (UTC offset) as +/-[hour]:[minute]");
+    io::stdin().read_line(&mut buffer).expect("failed to read line");
+    let offset = buffer.clone();
+    buffer.clear();
+
+    let unix = unix_from_datetime(date.trim(), time.trim(), offset.trim());
 
     println!("<t:{}>", unix);
     println!("<t:{}:R>", unix);
 }
 
-fn unix_from_datetime(date:&str, time:&str, offset_h:i8, offset_m:i8) -> i64{
+fn unix_from_datetime(date:&str, time:&str, offset:&str) -> i64{
     Date::parse(date, &DATE_FORMAT)
         .unwrap()
         .with_time(Time::parse(time, &TIME_FORMAT)
             .unwrap())
-        .assume_offset(UtcOffset::from_hms(offset_h,offset_m,0)
+        .assume_offset(UtcOffset::parse(offset, &OFFSET_FORMAT)
             .unwrap())
         .unix_timestamp()
 }
