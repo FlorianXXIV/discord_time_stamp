668667c628eac709cdf93253ea0c0571279f597f
Florian Kokotz <f.kokotz@tu-braunschweig.de>
2023-07-05 09:15:07 +0200

    added functions to get discord unix timestamp from strings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
-use std::ffi::c_int;
-use std::io;
+use time::{Date, Time, UtcOffset};
+use time::format_description::FormatItem;
+use time::macros::format_description;
 
+const DATE_FORMAT:&[FormatItem] = format_description!("[year]-[month]-[day]");
+const TIME_FORMAT:&[FormatItem] = format_description!("[hour]:[minute]");
 fn main() {
-    println!("Guess the Number!");
+    let date = "2023-07-05";
+    let time = "09:45";
+    let offset_h = 2;
+    let offset_m = 0;
 
-    println!("Please input your guess.");
+    let unix = unix_from_datetime(date, time, offset_h, offset_m);
 
-    let mut guess = String::new();
+    println!("<t:{}>", unix);
+    println!("<t:{}:R>", unix);
+}
 
-    let number: c_int;
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read Line");
-
-    println!("You guessed: {guess}");
+fn unix_from_datetime(date:&str, time:&str, offset_h:i8, offset_m:i8) -> i64{
+    Date::parse(date, &DATE_FORMAT)
+        .unwrap()
+        .with_time(Time::parse(time, &TIME_FORMAT)
+            .unwrap())
+        .assume_offset(UtcOffset::from_hms(offset_h,offset_m,0)
+            .unwrap())
+        .unix_timestamp()
 }
